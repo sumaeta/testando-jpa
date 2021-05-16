5efd588b9c14aaf1ef649245e18c38b3ac64e7ec
sumaeta <brenosumaeta33@gmail.com>
2021-05-15 21:25:54 -0400

    Buscando por salário

--- src/main/java/com/test/jpa/controllers/UserController.java
+++ src/main/java/com/test/jpa/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.jpa.entities.User;
@@ -29,6 +30,12 @@ public class UserController {
 	@GetMapping(value = "/page")
 	public ResponseEntity<Page<User>> findAll(Pageable pageable) {
 	    Page<User> result = repository.findAll(pageable);
+	    return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping(value = "/search-salary")
+	public ResponseEntity<Page<User>> searchBySalary(@RequestParam(defaultValue = "0") Double minSalary, @RequestParam(defaultValue = "1000000000000") Double maxSalary, Pageable pageable) {
+	    Page<User> result = repository.searchBySalary(minSalary, maxSalary, pageable);
 	    return ResponseEntity.ok(result);
 	}
 	
--- src/main/java/com/test/jpa/repositories/UserRepository.java
+++ src/main/java/com/test/jpa/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package com.test.jpa.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.test.jpa.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+
+	@Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
+	Page<User> searchBySalary(Double minSalary, Double maxSalary, Pageable pageable);
 
 }
